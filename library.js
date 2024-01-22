134c88bed39f49f0478d37e41060d9084d0aec04
Adithya Ajith <aajith@kantime.com>
2024-01-22 07:58:23 -0600

    Add HTML elements for books and update them when removed

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const libElem = document.querySelector("#book-cards");
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -16,8 +18,48 @@ function Book(title, author, pages, read) {
         }
         return this.title + " by " + this.author + ", " + pages + readStatus;
     }
+
+    this.toggleRead = () => {
+        this.read = !this.read;
+    }
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.unshift(new Book(title, author, pages, read));
+    renderLibrary();
 }
+
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    renderLibrary();
+}
+
+function renderLibrary() {
+    let cardElems = [];
+    let index = 0;
+    myLibrary.forEach((book) => {
+        // Create header and p tags for book information
+        let card = document.createElement("div");
+        card.className = "book-card";
+        card.textContent = book.title;
+        card.dataset.index = index;
+        
+        let delSelf = document.createElement("button");
+        delSelf.textContent = "Delete Book";
+        delSelf.addEventListener("click", function(e) {
+            removeBookFromLibrary(e.target.parentElement.getAttribute("data-index"));
+        });
+
+        card.appendChild(delSelf);
+
+        cardElems.push(card);
+        index++;
+    })
+
+    libElem.replaceChildren(...cardElems);
+}
+
+addBookToLibrary("20000 leagues", "Jules Verne", "300", true);
+addBookToLibrary("alpha", "Jules Verne", "300", true);
+addBookToLibrary("beta leagues", "Jules Verne", "300", true);
+addBookToLibrary("charlie leagues", "Jules Verne", "300", true);
