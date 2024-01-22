6b41d060d0a3de9b38d460d4cf6e40f610b16a31
Adithya Ajith <aajith@kantime.com>
2024-01-22 09:13:54 -0600

    Add basic card styling and add read status functionality

--- library.js
+++ library.js
@@ -41,8 +41,51 @@ function renderLibrary() {
         // Create header and p tags for book information
         let card = document.createElement("div");
         card.className = "book-card";
-        card.textContent = book.title;
         card.dataset.index = index;
+
+        // Add card header
+        let header = document.createElement("div");
+        header.className = "card-header";
+        let bookTitle = document.createElement("h1");
+        bookTitle.textContent = book.title;
+        let authorName = document.createElement("h2");
+        authorName.textContent = book.author;
+        
+        header.appendChild(bookTitle);
+        header.appendChild(authorName);
+        card.appendChild(header);
+
+        // Add card info
+        let bookInfo = document.createElement("div");
+        bookInfo.className = "card-body";
+        let numPages = document.createElement("p");
+        numPages.textContent = book.pages + " pages";
+
+        bookInfo.appendChild(numPages);
+        card.appendChild(bookInfo);
+
+        // Add "read" status checkbox
+        let readDiv = document.createElement("div");
+        let readLabel = document.createElement("label");
+        let readBox = document.createElement("input");
+        
+        readDiv.className = "read-div";
+        readLabel.className = "slider";
+        readBox.id = "read-box-" + index;
+        readLabel.htmlFor = readBox.id;
+        readBox.type = "checkbox";
+        readBox.checked = book.read;
+        readLabel.textContent = "Read?";
+
+        readBox.addEventListener("change", function(e) {
+            myLibrary[e.target.parentElement.parentElement.parentElement.getAttribute("data-index")].read = e.target.checked;
+            console.dir(myLibrary);
+        });
+
+        readDiv.appendChild(readLabel);
+        readDiv.appendChild(readBox);
+        bookInfo.appendChild(readDiv);
+
         
         let delSelf = document.createElement("button");
         delSelf.textContent = "Delete Book";
@@ -59,7 +102,7 @@ function renderLibrary() {
     libElem.replaceChildren(...cardElems);
 }
 
-addBookToLibrary("20000 leagues", "Jules Verne", "300", true);
+addBookToLibrary("20000 leagues under teh sesa", "Jules Verne", "300", true);
 addBookToLibrary("alpha", "Jules Verne", "300", true);
 addBookToLibrary("beta leagues", "Jules Verne", "300", true);
 addBookToLibrary("charlie leagues", "Jules Verne", "300", true);
